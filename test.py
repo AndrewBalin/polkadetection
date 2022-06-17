8dc9e190dcb1cb8548739536b6f5be3ad0cf7192
DoneGame <d.geyzha@gmail.com>
2022-06-18 01:54:58 +0500

    testing

---
+++ test.py
@@ -0,0 +1,36 @@
+import random
+import os
+import torch.nn as nn
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+
+
+def init():
+    # Model initialization
+    return Img2Vec(cuda=True, model='resnet18')
+
+
+def img_to_vec(img, model):
+    # Transforming
+    return model.get_vec(img, tensor=True)
+
+
+model = init()
+
+dataset = 'C:/Users/Home-PC/Downloads/train_dataset_1/'
+n = 20
+
+cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+a = []
+for _ in range(10):
+    rand_products = random.sample(range(0, 2388), n)
+    for i in rand_products:
+        for j in range(len(os.listdir(dataset + str(i))) // 2):
+            try:
+                img1 = Image.open(dataset + str(i) + '/' + str(2 * j) + '.jpg')
+                img2 = Image.open(dataset + str(i) + '/' + str(2 * j + 1) + '.jpg')
+                a.append(cos(img_to_vec(img1, model), img_to_vec(img2, model)))
+            except:
+                continue
+
+print(sum(a) / len(a))
